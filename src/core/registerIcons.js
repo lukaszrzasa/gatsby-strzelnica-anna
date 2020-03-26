af8c50ab8377077f0703595e09a8e454309d06bc
= <lukasz@lukaszrzasa.pl>
2020-03-26 19:37:16 +0100

    add more icons

--- src/core/registerIcons.js
+++ src/core/registerIcons.js
@@ -1,10 +1,31 @@
-import { faStar, faStarHalf, faTimes } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faBullseye, faCalendarAlt,
+  faDollarSign, faHistory,
+  faHome, faIdCard, faInfoCircle, faMedal,
+  faStar,
+  faStarHalf,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 export const registerIcons = () => {
   library.add(
     faStarHalf,
     faTimes,
     faStar,
+    faTimes,
+    faInstagram,
+    faFacebook,
+    faYoutube,
+    faBars,
+    faHome,
+    faDollarSign,
+    faBullseye,
+    faCalendarAlt,
+    faMedal,
+    faIdCard,
+    faInfoCircle,
   );
 };
